a16cb363abc0b21ad21fa62ef737b2caa18c59aa
Volha Valasevich <Volha_Valasevich@epam.com>
2019-02-04 10:13:33 +0300

    test drag and drop

--- index.js
+++ index.js
@@ -1,12 +1,4 @@
 const Steps = require("./steps.js");
 const steps = new Steps();
 
-steps.BasicAuthorization().then(() => {
-    return steps.DragAndDrop();
-}).then(() => {
-    return steps.FloatingMenu();
-}).then(() => {
-    return steps.KeyPress();
-}).then(() => {
-    return steps.ExitIntent();
-});
+steps.DragAndDrop();
